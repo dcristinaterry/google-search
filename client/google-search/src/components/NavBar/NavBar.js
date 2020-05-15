c8d9e824ac5e7ac0bc684d366584ec1624ba9b0b
guachochi <davis.bradleyj@gmail.com>
2020-05-15 12:53:35 -0700

    add navbar, jumbotron pages/folders and styling

---
+++ client/google-search/src/components/NavBar/NavBar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <ul className="nav">
+                <li className="nav-item">
+                    <a className="nav-link" href="/">Search</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="/saved">Saved</a>
+                </li>
+            </ul>
+        </nav>
+    )
+};
+
+export default Navbar;
