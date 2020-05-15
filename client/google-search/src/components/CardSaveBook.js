348fc96af4a24d9dbe617d7ddf71f3adf94ef3ca
CrisTerry <crisdc88@gmail.com>
2020-05-15 13:19:00 -0700

    Add CardSaveBook and CardSearchBook components

---
+++ client/google-search/src/components/CardSaveBook.js
@@ -0,0 +1,37 @@
+import React from "react"
+
+const CardSaveBook = (props) => {
+
+    return (
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">{props.title}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">{props.author}</h6>
+
+                <div class="card mb-3" style="max-width: 540px;">
+                    <div class="row no-gutters">
+                        <div class="col-md-4">
+                            <img src="..." class="card-img" alt="..."/>
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <p class="card-text">{props.body}</p>
+                                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            </div>
+                         </div>
+                    </div>
+                 </div>
+
+                 <button>
+                     view
+                 </button>
+                 <button>
+                     delete
+                 </button>
+            </div>
+        </div>
+    )
+
+
+
+}
