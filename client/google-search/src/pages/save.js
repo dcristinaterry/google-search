432d9a93ff455cd93037831dbc153f16a4547580
guachochi <davis.bradleyj@gmail.com>
2020-05-15 14:03:23 -0700

    save page delete button

--- client/google-search/src/pages/save.js
+++ client/google-search/src/pages/save.js
@@ -1,9 +1,58 @@
 import React, { Component } from "react";
-import DeleteBtn from "../components/DeleteBtn";
-import Jumbotron from "../components/Jumbotron";
-import Navbar from "../"
+import Jumbotron from "../components/Jumbotron/Jumbotron";
+import Navbar from "../components/NavBar/NavBar";
 import API from "../utils/API";
 import { Link } from "react-router-dom";
-import { Col, Row, Container } from "../components/Grid";
-import { List, ListItem } from "../components/List";
-import { Input, TextArea, FormBtn } from "../components/Form";
+
+class SavePage extends Component() {
+
+    state = {
+        books: []
+    }
+
+    componentDidMount() {
+        this.loadBooks();
+    }
+
+    loadBooks = () => {
+        API.getBooks()
+            .then(res =>
+                this.setState({ books: res.data })
+            )
+            .catch(err => console.log(err));
+    };
+
+    deleteBook = id => {
+        API.deleteBook(id)
+            .then(res => this.loadBooks())
+            .catch(err => console.log(err));
+    };
+
+    handleInputChange = event => {
+        const name = event.target.name,
+        const value = event.target.value
+        this.setState({
+            [name]: value
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <Navbar>
+                    <Jumbotron>
+                        <CardSaveBook />
+                    </Jumbotron>
+                </Navbar>
+
+            </div>
+
+
+
+        );
+    }
+
+
+}
+
+export default SavePage;
