be04b71d4565d2b5c56506f80fa7e2b0f6251611
guachochi <davis.bradleyj@gmail.com>
2020-05-18 13:26:10 -0700

    merge with master

--- client/google-search/src/components/CardSearchBook.js
+++ client/google-search/src/components/CardSearchBook.js
@@ -16,7 +16,7 @@ const CardSearchBook = (props) => {
     }
 
     return (
-        <div className="card" style="width: 18rem;">
+        <div className="card" style={{width: "18rem"}}>
             <div className="card-body">
                 <h5 className="card-title">{props.title}</h5>
                 <h6 className="card-subtitle mb-2 text-muted">{props.author}</h6>
